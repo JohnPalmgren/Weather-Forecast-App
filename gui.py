4db35d1422816e00645334b2e62d65226e46f486
John <johnmpalmgren@gmail.com>
2020-08-17 19:24:40 +0100

    offline branch created

--- gui.py
+++ gui.py
@@ -4,6 +4,8 @@ from PIL import ImageTk, Image
 import process
 import api
 
+### img works -- put into all frames... 
+
 APIkey = '940b74af-8410-4516-9326-e39b07de8cdd'
 interface = api.API(APIkey)
 
@@ -11,6 +13,10 @@ root = Tk()
 
 my_label = Label(root, text='5 day forcast')
 my_label.grid(row=0, column=0)
+
+def images(code):
+
+    return ImageTk.PhotoImage(Image.open(f'icons/{code}.ico'))
 
 
 notebook = ttk.Notebook(root)
@@ -21,26 +27,34 @@ frame2 = Frame(notebook, width=500, height=500)
 frame3 = Frame(notebook, width=500, height=500)
 frame4 = Frame(notebook, width=500, height=500)
 frame5 = Frame(notebook, width=500, height=500)
+test = Frame(notebook, width=500, height=500)
 
 frame1.grid(row=2, column=0)
 frame2.grid(row=2, column=0)
 frame3.grid(row=2, column=0)
 frame4.grid(row=2, column=0)
 frame5.grid(row=2, column=0)
+test.grid(row=2, column=0)
 
 notebook.add(frame1, text=process.tabs()[0])
 notebook.add(frame2, text=process.tabs()[1])
 notebook.add(frame3, text=process.tabs()[2])
 notebook.add(frame4, text=process.tabs()[3])
 notebook.add(frame5, text=process.tabs()[4])
+notebook.add(test, text='test')
 
+
+img = (images(3))
+
+testlab = Label(test, image=img)
+testlab.grid(row=0, column=0)
 
 user_input = Entry(root, width=60)
 user_input.grid(row=1, column=0)
 
 
 def click():
-    pass
+
     siteid = interface.get_site_id_from_user_input(user_input.get())
     api_data = interface.get_data_from_api(siteid)
     data = process.split_lists(api_data, 5)
@@ -49,7 +63,9 @@ def click():
     
     day5_time = Label(frame5, text=time)
     day5_temp = Label(frame5, text=data[13])
-    day5_weather = Label(frame5, text=data[14])
+    img = images(3)#data[14][0])
+    day5_weather = Label(frame5, image=img)
+    print(data[14][0])
     day5_temp.grid(row=0, column=0)
     day5_weather.grid(row=1, column=0)
     day5_time.grid(row=2, column=0)
@@ -86,35 +102,18 @@ search = Button(root, text='search', command=click)
 search.grid(row=1, column=1)
 
 
-def images(code):
-    root.iconbitmap('C:\\Users\\johnm\\Documents\\data_science\\coding\\projects'
-                  '\\weather_gui\\icons') #change for github
-    
-    #img = Image.open(f'{code}.png')
-    img = Image.open('0.png')
-    
-    resize = img.resize((120,60), Image.ANTIALIAS)
-    
-    return ImageTk.PhotoImage(resize)
+def split_list(listy): #for lambda
+    for i in listy:
+        return i
 
 
-# root.iconbitmap('C:\\Users\\johnm\\Documents\\data_science\\coding\\projects'
-#              '\\weather_gui') #change for github
-    
-#     #img = Image.open(f'{code}.png')
-# img = Image.open('test_img.png')
-    
-# resize = img.resize((120,60), Image.ANTIALIAS)
-    
-# new_img = ImageTk.PhotoImage(resize)
 
-# piclabel = Label(root, image=new_img)
-# piclabel.grid(row=3, column=2)
 
-img = images(0)
 
-img_lable1 = Label(root, image=img)
-img_lable1.grid(row=0, column=0)
+# img = images(5)
+
+# img_lable1 = Label(root, image=img)
+# img_lable1.grid(row=0, column=0)
 
 # root.iconbitmap('C:\\Users\\johnm\\Documents\\data_science\\coding\\projects'
 #                 '\\weather_gui') #change for github
